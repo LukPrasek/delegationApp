1a60d0d04214161e925885d968c0a37cd02cfeaa
LukPrasek <lukasz.prasek@gmail.com>
2018-10-09 23:39:15 +0200

    added PassengerService class

--- src/main/java/DelegationAppApplication.java
+++ src/main/java/DelegationAppApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import pl.lukaszprasek.delegationApp.domain.repositories.EmployeeRepository;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @ComponentScan(basePackages = "pl.lukaszprasek.delegationApp")
 @EntityScan(basePackages = "pl.lukaszprasek.delegationApp.domain.entities")
+@EnableSwagger2
 @EnableJpaRepositories(basePackageClasses = EmployeeRepository.class)
 public class DelegationAppApplication {
 
